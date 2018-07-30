51b2916f1e3e70f0228064da5f86df372e0b920e
Gilvam Mourão <gilvamm@gmail.com>
2018-07-30 15:05:32 -0300

    created instantiation operation

--- src/main/java/com/gilvam/cursomc/CursomcApplication.java
+++ src/main/java/com/gilvam/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.gilvam.cursomc;
 
+import com.gilvam.cursomc.domain.Category;
+import com.gilvam.cursomc.repositories.CategoryRepository;
+import com.gilvam.cursomc.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		List<Category> categories = new ArrayList<>();
+		categories.add(new Category(null, "Informática"));
+		categories.add(new Category(null, "Escritório"));
+
+		categoryRepository.saveAll(categories);
 	}
 }
