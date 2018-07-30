36ba287c526f25ee4cf4871a0064a789e0b04f3a
Gilvam Mourão <gilvamm@gmail.com>
2018-07-30 14:01:54 -0300

    Data base H2 and automatic creation database

--- src/main/java/com/gilvam/cursomc/domain/Category.java
+++ src/main/java/com/gilvam/cursomc/domain/Category.java
@@ -1,12 +1,21 @@
 package com.gilvam.cursomc.domain;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
 
 	public Category() {
