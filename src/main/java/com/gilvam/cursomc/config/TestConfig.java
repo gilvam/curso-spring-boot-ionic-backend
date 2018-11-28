5b8da991453838140ae1b0e9c42cd82479f2f956
Gilvam Mourão <gilvamm@gmail.com>
2018-11-28 08:27:51 -0200

    Criando profile de teste

---
+++ src/main/java/com/gilvam/cursomc/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.gilvam.cursomc.config;
+
+import com.gilvam.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        this.dbService.instantiateTestDatabase();
+
+        return true;
+    }
+}
